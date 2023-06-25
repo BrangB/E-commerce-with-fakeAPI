d2a5d0889d38015d300c4743b9ff283f2aab5f85
Brag <brangtsawmaung89@gmail.com>
2023-06-25 17:51:18 +0630

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+// import router from router dom
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+// import pages
+import Home from './pages/Home'
+import ProductDetails from './pages/ProductDetails'
+
+//import components
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Slidebar from './components/Sidebar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="overflow-hidden">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/product/:id' element={<ProductDetails />}/>
+        </Routes>
+        <Slidebar />
+        <Footer />
+      </Router>
     </div>
   );
 }
